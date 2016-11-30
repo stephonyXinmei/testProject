25f6e28c5eccf65d49058c10738d0852830f6cef
stephonyXinmei <rui.chen@xinmei365.com>
2016-11-30 14:16:41 +0800

    1

--- src/main/scala-2.11/lda/LatentDirichletAllocationExample.scala
+++ src/main/scala-2.11/lda/LatentDirichletAllocationExample.scala
@@ -1,10 +1,13 @@
 package lda
 
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+
+import scala.collection.mutable
 
 // $example on$
 import org.apache.spark.mllib.clustering.{DistributedLDAModel, LDA}
-import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.linalg.{Vectors, Vector}
 // $example off$
 
 object LatentDirichletAllocationExample {
@@ -13,6 +16,7 @@ object LatentDirichletAllocationExample {
 
     val conf = new SparkConf().setAppName("LatentDirichletAllocationExample")
     val sc = new SparkContext(conf)
+    /*
 
     val hdfs_dir = "hdfs:///hbase2hdfs/testLDA"
 
@@ -41,6 +45,53 @@ object LatentDirichletAllocationExample {
     //   "target/lda/LatentDirichletAllocationExample/LDAModel")
 
     sc.stop()
+    */
+
+    // Load documents from text files, 1 document per file
+    val corpus: RDD[String] = sc.wholeTextFiles("docs/*.md").map(_._2)
+
+    // Split each document into a sequence of terms (words)
+    val tokenized: RDD[Seq[String]] =
+      corpus.map(_.toLowerCase.split(" ")).map(_.filter(_.length > 3).filter(_.forall(java.lang.Character.isLetter)))
+
+    // Choose the vocabulary.
+    //   termCounts: Sorted list of (term, termCount) pairs
+    val termCounts: Array[(String, Long)] =
+    tokenized.flatMap(_.map(_ -> 1L)).reduceByKey(_ + _).collect().sortBy(-_._2)
+    //   vocabArray: Chosen vocab (removing common terms)
+    val numStopwords = 20
+    val vocabArray: Array[String] =
+      termCounts.takeRight(termCounts.size - numStopwords).map(_._1)
+    //   vocab: Map term -> term index
+    val vocab: Map[String, Int] = vocabArray.zipWithIndex.toMap
+
+    // Convert documents into term count vectors
+    val documents: RDD[(Long, Vector)] =
+      tokenized.zipWithIndex.map { case (tokens, id) =>
+        val counts = new mutable.HashMap[Int, Double]()
+        tokens.foreach { term =>
+          if (vocab.contains(term)) {
+            val idx = vocab(term)
+            counts(idx) = counts.getOrElse(idx, 0.0) + 1.0
+          }
+        }
+        (id, Vectors.sparse(vocab.size, counts.toSeq))
+      }
+
+    // Set LDA parameters
+    val numTopics = 10
+    val lda = new LDA().setK(numTopics).setMaxIterations(10)
+
+    val ldaModel = lda.run(documents)
+
+    // Print topics, showing top-weighted 10 terms for each topic.
+    val topicIndices = ldaModel.describeTopics(maxTermsPerTopic = 10)
+    topicIndices.foreach { case (terms, termWeights) =>
+      println("TOPIC:")
+      terms.zip(termWeights).foreach { case (term, weight) =>
+        println(s"${vocabArray(term.toInt)}\t$weight")
+      }
+      println()
   }
 }
 // scalastyle:on println
