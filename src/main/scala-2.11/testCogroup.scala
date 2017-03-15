5a61ba85e61c3de95c819a30c149b6f10837e2ad
stephonyXinmei <rui.chen@xinmei365.com>
2017-03-15 11:08:46 +0800

    test scala cogroup

---
+++ src/main/scala-2.11/testCogroup.scala
@@ -0,0 +1,25 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by xm on 15/03/2017.
+  */
+object testCogroup {
+
+  def main(args: Array[String]) {
+
+    val conf = new SparkConf().setAppName("cogroup-example")
+    val sc = new SparkContext(conf)
+
+    val data1 = sc.parallelize(List((1, "www"), (2, "bbs")))
+
+    val data2 = sc.parallelize(List((1, "iteblog"), (2, "very")))
+
+    val result = data1.cogroup(data2).collect()
+
+    print(result)
+
+    sc.stop()
+
+  }
+
+}
